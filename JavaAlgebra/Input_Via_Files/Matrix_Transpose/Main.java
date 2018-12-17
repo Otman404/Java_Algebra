6b2df5f2728317d46f082019ea4b50f773d8a9e9
Otman404 <outhmaneboughaba98@gmail.com>
2018-12-16 23:06:12 -0800

    Done

--- JavaAlgebra/Input_Via_Files/Matrix_Transpose/Main.java
+++ JavaAlgebra/Input_Via_Files/Matrix_Transpose/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Main{
+class Main{ 
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		System.out.println("\n\t\t\t======== Matrix Transpose ========");
