3089e830a258d5ac690e015622c53559cd835109
Otman404 <outhmaneboughaba98@gmail.com>
2018-12-16 23:00:58 -0800

    Done

--- JavaAlgebra/Input_Via_Files/Matrix_Substraction/Main.java
+++ JavaAlgebra/Input_Via_Files/Matrix_Substraction/Main.java
@@ -1,28 +1,22 @@
 import java.util.*;
+import java.io.*;
+
 class Main{
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("\n\t\t\t======== Matrix Multiplication ========");
-		System.out.println("Matrix 1 rows : ");
-		int rows1 = in.nextInt();
-		System.out.println("Matrix 1 Columns : ");
-		int columns1 = in.nextInt();
-		Matrix m1 = new Matrix(rows1,columns1);
-		System.out.println("Matrix 2 rows : ");
-		int rows2 = in.nextInt();
-		System.out.println("Matrix 2 Columns : ");
-		int columns2 = in.nextInt();
-		int size1 = columns1*rows1;
-		int size2 = columns2*rows2;
+	public static void main(String[] args) throws FileNotFoundException {
+
+		System.out.println("\n\t\t\t======== Matrix Addition ========");
+		Matrix m1 = new Matrix("matrix1.txt");
+		Matrix m2 = new Matrix("matrix2.txt");
+		m1.setMat();
+		m2.setMat();
+		Matrix result = new Matrix(m1.getRows(),m1.getColumns());
+		int size1 = m1.getColumns()*m1.getRows();
+		int size2 = m2.getColumns()*m2.getRows();
 		if(size1 != size2){
 			System.out.println("The number of columns in the first matrix should be equal to the number of rows in the second.");
 			return;
 		}
-		Matrix m2 = new Matrix(rows2,columns2);
-		Matrix result = new Matrix(rows1,columns1);
-		m1.setMat();
-		m2.setMat();
 		System.out.println("Matrix 1");
 		m1.Display();
 		System.out.println("Matrix 2");
@@ -30,15 +24,15 @@ class Main{
 		Thread[] threads = new Thread[size1];
 		int k = 0;
 		try{
-			for (int i = 0 ;i<rows1;i++ ){
-				for (int j = 0;j<columns2 ;j++ ) {
+			for (int i = 0 ;i<m1.getRows();i++ ){
+				for (int j = 0;j<m2.getColumns() ;j++ ) {
 					threads[k] = new Thread(new MatrixSubstraction(m1,m2,result,i,j));
 					threads[k].start();
 					threads[k].join();
 					k++;
 				}
 			}
-		}catch(Exception e){}		
+		}catch(Exception e){}
 
 		System.out.println("Result : ");
 		result.Display();
