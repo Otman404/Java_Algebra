b441e9005c8365721a71679b8249e5e7bbe65a5b
Otman404 <outhmaneboughaba98@gmail.com>
2018-12-16 22:47:22 -0800

    Almost done

--- JavaAlgebra/Input_Via_Files/Scalar_Matrix_Multip/Main.java
+++ JavaAlgebra/Input_Via_Files/Scalar_Matrix_Multip/Main.java
@@ -1,27 +1,25 @@
 import java.util.*;
+import java.io.*;
+
 class Main{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException{
+		System.out.println("\n\t\t\t======== Scalar-Matrix Multiplication ========");
 		Scanner in = new Scanner(System.in);
-		System.out.println("\n\t\t\t======== Matrix Multiplication ========");
-		System.out.println("Matrix rows : ");
-		int rows = in.nextInt();
-		System.out.println("Matrix Columns : ");
-		int columns = in.nextInt();
-		Matrix m1 = new Matrix(rows,columns);
-		Matrix result = new Matrix(rows,columns);
-		m1.setMat();
+		Matrix m = new Matrix("matrix.txt");
+		Matrix result = new Matrix(m.getRows(),m.getColumns());
+		m.setMat();
 		System.out.println("Enter a Number : ");
 		int scalar = in.nextInt();
-		System.out.println("Matrix 1");
-		m1.Display();
-		int size = rows*columns;
+		System.out.println("Matrix : ");
+		m.Display();
+		int size = m.getRows()*m.getColumns();
 		Thread[] threads = new Thread[size];
 		int k = 0;
 		try{
-			for (int i = 0 ;i<rows;i++ ){
-				for (int j = 0;j<columns ;j++ ) {
-					threads[k] = new Thread(new ScalarByMatrix(m1,scalar,result,i,j));
+			for (int i = 0 ;i<m.getRows();i++ ){
+				for (int j = 0;j<m.getColumns() ;j++ ) {
+					threads[k] = new Thread(new ScalarByMatrix(m,scalar,result,i,j));
 					threads[k].start();
 					threads[k].join();
 					k++;
